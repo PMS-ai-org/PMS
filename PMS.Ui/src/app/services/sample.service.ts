c4c88bb607a3eb321dd122064419fa458c3d6cf2
SwapnilÂ Kumar Bhoir <SBhoir@pattersoncompanies.com>
2025-08-13 18:05:43 +0530

    Added lint and unit test case setup

--- PMS.Ui/src/app/services/sample.service.ts
+++ PMS.Ui/src/app/services/sample.service.ts
@@ -1,11 +1,11 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class SampleService {
-  private http = inject(HttpClient);
 
+  constructor(private httpClient: HttpClient) {}
   getItems() {
-    return this.http.get<{ id:number; name:string }[]>('/assets/data/items.json');
+    return this.httpClient.get<{ id:number; name:string }[]>('/assets/data/items.json');
   }
 }
